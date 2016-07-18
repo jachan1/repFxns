e0f430e37569834c8ccc9bd22e90eba8658c9678
jachan1 <jachan1@gmail.com>
2016-07-18 15:36:30 -0400

    Added ggplot theme

---
+++ R/theme_bg.R
@@ -0,0 +1,13 @@
+#' Biostats Group ggplot Theme
+#'
+#'
+#' theme with no background or gridlines just with x and y axis
+
+theme_bg <- function(){
+  theme(panel.border = element_blank(), 
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(), 
+      axis.line.x = element_line(colour = "gray20"),
+      axis.line.y = element_line(colour = "gray20"),
+      panel.background=element_blank())
+}
